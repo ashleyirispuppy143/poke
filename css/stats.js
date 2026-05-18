294d8e71799bfc5755d416c90696d7be3bdb0914
ashleyirispuppy <ashleyirispuppy@noreply.codeberg.org>
2026-05-19 00:14:13 +0200

    Update css/stats.js

--- css/stats.js
+++ css/stats.js
@@ -22,7 +22,7 @@
   } catch (e) {
     }
 
-   const url = "/static/improving-poke.js?v=453";
+   const url = "/static/improving-poke.js?v=4533";
   if (document.querySelector(`script[src="${url}"]`)) return;
 
   const s = document.createElement("script");
