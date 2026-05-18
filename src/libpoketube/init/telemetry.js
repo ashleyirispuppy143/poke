0ec95b831742a7e382089870d547fbe6bc79c932
ashleyirispuppy <ashleyirispuppy@noreply.codeberg.org>
2026-05-18 22:02:39 +0200

    Update src/libpoketube/init/telemetry.js

--- src/libpoketube/init/telemetry.js
+++ src/libpoketube/init/telemetry.js
@@ -1,7 +1,7 @@
 const fs = require("fs")
 const path = require("path")
 
-const telemetryConfig = { telemetry: true }
+const telemetryConfig = { telemetry: false }
 
 const statsFile = path.join(__dirname, "stats.json")
 const statsFileV2 = path.join(__dirname, "stats-v2.json")
