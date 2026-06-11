7bfd13373ee53a29c3baae9e80a52d45b36b795c
ashleyirispuppy <ashleyirispuppy@noreply.codeberg.org>
2026-06-12 01:10:50 +0200

    Update src/libpoketube/init/telemetry.js

--- src/libpoketube/init/telemetry.js
+++ src/libpoketube/init/telemetry.js
@@ -4,7 +4,7 @@ const path = require("path")
 
 const SETTINGS = Object.freeze({
   telemetry: {
-    enabled: false,
+    enabled: true,
     storage: {
       path: path.join(__dirname, "telemetry.json")
     },
